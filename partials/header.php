6ee932d6a073f4b2d79f4e27d4ba5f128b92a285
Orlanesthesie <orlane.r.6@gmail.com>
2024-01-24 09:07:09 +0100

    dernier push

--- partials/header.php
+++ partials/header.php
@@ -3,9 +3,8 @@
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title img src="../assets/images/logo.png">
-        InstaClone
-    </title>
+    <title> InstaClone </title>
+    <link rel="icon" type="image/x-icon" href="../assets/images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="/assets/css/style.css" rel="stylesheet">
     <!-- <script src="https://kit.fontawesome.com/23471b5a81.js" crossorigin="anonymous"></script> -->
